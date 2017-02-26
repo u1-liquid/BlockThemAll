3ea17f826b48392613bd34047711283e5fbb5508
spr0tsuki <satsuki.yanagi@gmail.com>
2017-02-26 18:27:01 +0900

    Set default value of AutoSwitchCredApiLimit as true

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace BlockThemAll.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool AutoSwitchCredApiLimit {
             get {
                 return ((bool)(this["AutoSwitchCredApiLimit"]));
